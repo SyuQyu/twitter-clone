0ce448d6869ba0f558db97e5da0e5ed8b2e1e9fd
panxzzlyn <panxzzlyn@gmail.com>
2022-10-25 20:50:27 +0700

    setup backend & update ui main content

--- app.vue
+++ app.vue
@@ -11,8 +11,8 @@
           </div>
 
           <!-- Main content -->
-          <main class="col-span-12 md:col-span-8 xl:col-span-6 bg-red-500">
-            <h1>test</h1>
+          <main class="col-span-12 md:col-span-8 xl:col-span-6">
+            <router-view />
           </main>
 
           <!-- Right sidebar -->
